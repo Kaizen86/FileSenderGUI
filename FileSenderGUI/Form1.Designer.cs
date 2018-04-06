af377134d7f9d6963c95f899278c079cee82fef5
floathandthing <springtrap.bite87@gmail.com>
2018-04-06 20:21:32 +0100

    Initial commit

--- FileSenderGUI/Form1.Designer.cs
+++ FileSenderGUI/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FileSenderGUI
 {
-    partial class Form1
+    partial class Window
     {
         /// <summary>
         /// Required designer variable.
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(278, 244);
+            this.Name = "Window";
+            this.Text = "File Sender";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
